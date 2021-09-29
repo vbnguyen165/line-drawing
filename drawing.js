39e277468f87ced73de0f31fe7d6188221a73a2a
vbnguyen165 <55001552+vbnguyen165@users.noreply.github.com>
2021-09-29 15:10:38 -0400

    Implemented color + clear functionality

--- drawing.js
+++ drawing.js
@@ -4,7 +4,21 @@ var gl;
 const MAX_POINTS = 50;
 var cursor;
 var current_point;
-var current_n_points = 0;
+var current_n_points = 1;
+var color_index = 0;
+
+var colors = [
+
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0)   // cyan
+
+];
+var selected_color = colors[color_index];
 window.onload = function init() {
     var canvas = document.getElementById( "gl-canvas" );
 
@@ -52,9 +66,6 @@ window.onload = function init() {
         is set to the first value in the list.
     */
     canvas.addEventListener("click", function(event){
-      // if (current_point !== null) {
-      //   var temp = current_point;
-      // }
       if (current_n_points < MAX_POINTS) {
         current_n_points++;
         gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
@@ -65,7 +76,7 @@ window.onload = function init() {
 
         gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
         var t = vec4(1.0, 0.0, 0.0, 1.0);
-        gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * (current_n_points - 2), flatten(t));
+        gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * (current_n_points - 2), flatten(selected_color));
 
         if (current_n_points >= 2) {
         cursor = null;
@@ -73,21 +84,18 @@ window.onload = function init() {
       }
 
     });
-    //   if (current_n_points >= 2) {
-    //     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    //     gl.bufferSubData(gl.ARRAY_BUFFER, 8*(current_n_points), flatten(temp));
-    //
-    //     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-    //     var t = vec4(1.0, 0.0, 0.0, 1.0);
-    //     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(current_n_points), flatten(t));
-    //
-    //   }
-    // } );
+
+    let color_droplist = document.getElementById("colors");
+
+    color_droplist.addEventListener("click", function() {
+       color_index = color_droplist.selectedIndex;
+       selected_color = colors[color_index];
+    });
 
     canvas.addEventListener("mousemove", function(mouseFollow){
-      if (current_n_points === 0) {
-        current_n_points = 1;
-      }
+      // if (current_n_points === 0) {
+      //   current_n_points = 1;
+      // }
       gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
       cursor = vec2(2*event.clientX/canvas.width-1,
         2*(canvas.height-event.clientY)/canvas.height-1);
@@ -95,7 +103,7 @@ window.onload = function init() {
 
       gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
       var t = vec4(1.0, 0.0, 0.0, 1.0);
-      gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * (current_n_points - 1), flatten(t));
+      gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec4'] * (current_n_points - 1), flatten(selected_color));
     });
 
     render();
@@ -127,4 +135,13 @@ function render() {
         render multiple times to show an animation.
     */
     window.requestAnimationFrame(render);
+}
+
+function reset() {
+  gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  current_point = null;
+  cursor = null;
+  current_n_points = 1;
+  render();
 }
